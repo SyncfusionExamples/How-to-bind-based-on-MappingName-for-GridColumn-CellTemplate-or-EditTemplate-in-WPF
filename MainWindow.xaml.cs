801cd56842bc6a23489d1633efd3e56cfb40d00b
Sreemon Premkumar M <sreemonpremkumar.m@syncfusion.com>
2024-04-09 16:53:04 +0530

    ES-577034 - Provided the sample for the below KB article

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SfDataGrid_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    #region Model Class
+    public class OrderInfo
+    {
+        string _ProductId;
+        string _ProductName;
+        int _SalesID;
+        int _Percent;
+        string _CustomerName;
+        string _CustomerId;
+
+        public string ProductId
+        {
+            get { return _ProductId; }
+            set { _ProductId = value; }
+        }
+
+        public string ProductName
+        {
+            get { return _ProductName; }
+            set { _ProductName = value; }
+        }
+
+        public int SalesID
+        {
+            get { return _SalesID; }
+            set { _SalesID = value; }
+        }
+
+        public int Percent
+        {
+            get { return _Percent; }
+            set { _Percent = value; }
+        }
+
+        public string CustomerName
+        {
+            get { return _CustomerName; }
+            set { _CustomerName = value; }
+        }
+
+        public string CustomerId
+        {
+            get { return _CustomerId; }
+            set { _CustomerId = value; }
+        }
+
+        public OrderInfo(string productId, string productName, int salesID, int percent, string customerName, string customerId)
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.SalesID = salesID;
+            this.Percent = percent;
+            this.CustomerName = customerName;
+            this.CustomerId = customerId;
+        }
+    }
+    #endregion
+
+    #region View Model Class
+    public class ViewModel
+    {
+        private ObservableCollection<OrderInfo> _orders;
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return _orders; }
+            set { _orders = value; }
+        }
+
+        public ViewModel()
+        {
+            _orders = new ObservableCollection<OrderInfo>();
+            this.GenerateOrders();
+        }
+
+        private void GenerateOrders()
+        {
+            _orders.Add(new OrderInfo("P1", "Hardware", 3232, 557, "Maria Anders", "ALFKI"));
+            _orders.Add(new OrderInfo("P2", "Software", 4545, 890, "Ana Trujilo", "ANATR"));
+            _orders.Add(new OrderInfo("P3", "Hardware", 6742, 476, "Antonio Moreno", "ANTON"));
+            _orders.Add(new OrderInfo("P4", "Software", 7844, 368, "Thomas Hardy", "AROUT"));
+            _orders.Add(new OrderInfo("P5", "Hardware", 1234, 980, "Christina Berglund", "BERGS"));
+            _orders.Add(new OrderInfo("P6", "Software", 7568, 457, "Hanna Moos", "BLAUS"));
+            _orders.Add(new OrderInfo("P7", "Hardware", 9785, 234, "Frederique Citeaux", "BLONP"));
+            _orders.Add(new OrderInfo("P8", "Software", 1231, 789, "Martin Sommer", "BOLID"));
+            _orders.Add(new OrderInfo("P9", "Hardware", 4236, 442, "Laurence Lebihan", "BONAP"));
+            _orders.Add(new OrderInfo("P10", "Software", 7369, 578, "Elizabeth Lincoln", "BOTTM"));
+        }
+    }
+    #endregion
+}
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# How-to-bind-based-on-MappingName-for-GridColumn-CellTemplate-or-EditTemplate-in-WPF
+# How to bind based on MappingName for GridColumn CellTemplate or EditTemplate in WPF ?
 This example illustrates how to bind the CellTemplate or EditTemplate based on mapping name.
+
+In SfDataGrid, you can load DataTemplates for columns using GridColumn.CellTemplate. The DataContext for the templates loaded inside Grid are the underlying business object corresponding to that row regardless of MappingName of the column. So it is not possible to bind the values based on the MappingName of the GridColumn for the CellTemplate or EditTemplate of GridColumn. So you have to define the template for each column, separately.
+
+The following section describes how to bind the DataTemplate of the GridCell based on MappingName of the column.
+
+You can achieve this requirement by setting GridColumn.SetCellBoundValue to “True” as in the following code snippet, so that the DataContext for the template is changed to DataContextHelper from underlying record of the row. DataContextHelper contains the following properties.
+
+Value – Value of the property GridColumn.MappingName in the underlying record.
+
+Record – Underlying record of the row where the cell is present.
+
+So by this concept, you can use the same template for more than one column, as DataContextHelper returns the value from record based on MappingName of the column.
+
+The following code demonstrates how to bind the Value in DataTemplate.
+
+```XAML
+
+<Window.Resources>
+    <DataTemplate x:Key="celltemplate">
+    <TextBlock Text="{Binding Path=Value}" FontWeight="Bold" Foreground="Red"/>
+</DataTemplate>
+</Window.Resources>
+```
+The following code demonstrates how to set the DataTemplate key for the CellTemplate in GridColumn.
+
+```XAML
+
+<syncfusion:SfDataGrid.Columns>
+    <syncfusion:GridTextColumn MappingName="ProductId"/>
+    <syncfusion:GridTextColumn MappingName="ProductName"/>
+    <syncfusion:GridTextColumn MappingName="SalesID" SetCellBoundValue="True" CellTemplate="{StaticResource celltemplate}"/>
+    <syncfusion:GridTextColumn MappingName="Percent" SetCellBoundValue="True" CellTemplate="{StaticResource celltemplate}"  />
+    <syncfusion:GridTextColumn MappingName="CustomerName" />
+    <syncfusion:GridTextColumn MappingName="CustomerId"/>
+</syncfusion:SfDataGrid.Columns>
+```
+The following screenshot demonstrates the data template binding based on MappingName of the column. So, you can use the same template for different columns of SfDataGrid.
+
+![alt text](image.png)
+ 
+
+
+
+ 
+ 
